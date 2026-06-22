04b312a6f9ef6aaa5d4be31e2a8c4584214934fd
Saulo Brustolin <saulobrustolin@icloud.com>
2026-06-22 17:18:07 -0300

    fix infos

--- init-db.js
+++ init-db.js
@@ -1,8 +1,8 @@
 db = db.getSiblingDB('final-project');
 
 db.Users.insert({
-    name: 'Saulo Brustolin',
-    email: 'saulobrustolin@icloud.com',
+    name: 'João Gomes',
+    email: 'joaogomes@gmail.com',
     cpf: '302.408.410-00',
     balance: 0,
     password: '$2a$10$kcvWJuj9ySzuR9vYM0P6c.pV0LgO.geJKYc33CzqssYo4/iQkRsqu',
