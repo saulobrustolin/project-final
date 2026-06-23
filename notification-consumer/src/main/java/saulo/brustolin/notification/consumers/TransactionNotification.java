58240045885f5d2bbfda308e89aadcfac555946c
Saulo Brustolin <saulobrustolin@icloud.com>
2026-06-23 09:29:41 -0300

    fix resendconfig

--- notification-consumer/src/main/java/saulo/brustolin/notification/consumers/TransactionNotification.java
+++ notification-consumer/src/main/java/saulo/brustolin/notification/consumers/TransactionNotification.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import com.resend.Resend;
 import com.resend.services.emails.model.CreateEmailOptions;
 
 import lombok.AllArgsConstructor;
@@ -24,7 +23,7 @@ import saulo.brustolin.notification.models.TransactionEvent;
 @AllArgsConstructor
 public class TransactionNotification {
 
-    private final Resend resend;
+    private final ResendConfig resendConfig;
     private final TemplateEngine templateEngine;
     private final Locale locale = Locale.of("pt", "BR");;
     private final NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
@@ -45,14 +44,14 @@ public class TransactionNotification {
         String htmlContent = templateEngine.process("transaction-created", context);
 
         CreateEmailOptions params = CreateEmailOptions.builder()
-            .from(String.format("criação de transação. <%s>", ResendConfig.EMAIL_FROM))
+            .from(String.format("criação de transação. <%s>", resendConfig.getEmailFrom()))
             .to(message.email())
             .subject("uma nova transação foi criada.")
             .html(htmlContent)
             .build();
 
         try {
-            resend.emails().send(params);
+            resendConfig.resendClient().emails().send(params);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -74,14 +73,14 @@ public class TransactionNotification {
         String htmlContent = templateEngine.process("transaction-updated", context);
 
         CreateEmailOptions params = CreateEmailOptions.builder()
-            .from(String.format("atualização de transação. <%s>", ResendConfig.EMAIL_FROM))
+            .from(String.format("atualização de transação. <%s>", resendConfig.getEmailFrom()))
             .to(message.email())
             .subject("uma transação existente foi atualizada.")
             .html(htmlContent)
             .build();
 
         try {
-            resend.emails().send(params);
+            resendConfig.resendClient().emails().send(params);
         } catch (Exception e) {
             e.printStackTrace();
         }
