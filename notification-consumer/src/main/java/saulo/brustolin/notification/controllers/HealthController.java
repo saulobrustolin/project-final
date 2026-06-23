c7b10a8a7feb73aadfcc72dbec7393074ee681bc
Saulo Brustolin <saulobrustolin@icloud.com>
2026-06-23 08:24:05 -0300

    adding /health route in notification and fix function name in api

--- notification-consumer/src/main/java/saulo/brustolin/notification/controllers/HealthController.java
+++ notification-consumer/src/main/java/saulo/brustolin/notification/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package saulo.brustolin.project.controllers;
+package saulo.brustolin.notification.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    
+
     @GetMapping(path = "/health")
     public ResponseEntity<Void> health() {
         return ResponseEntity.ok().build();
