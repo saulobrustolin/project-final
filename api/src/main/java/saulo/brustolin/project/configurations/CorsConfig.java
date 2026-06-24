a67e48621b78cb8250cfe8f31cc849d82479e1c5
boust <saulobrustolin@icloud.com>
2026-06-23 21:45:49 -0300

    create dinamic origins cors

--- api/src/main/java/saulo/brustolin/project/configurations/CorsConfig.java
+++ api/src/main/java/saulo/brustolin/project/configurations/CorsConfig.java
@@ -1,5 +1,6 @@
 package saulo.brustolin.project.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${api.cors}")
+    private String origins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:80", "http://localhost")
+                        .allowedOrigins(origins.split(","))
                         .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
